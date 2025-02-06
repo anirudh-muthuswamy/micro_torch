e29f2d50ea7924d42512222c45ac1b782e856cc5
Anirudh Muthuswamy <126427302+anirudh-muthuswamy@users.noreply.github.com>
2025-02-06 00:14:54 -0500

    added more math op methods and fixed error with exp

--- micrograd.py
+++ micrograd.py
@@ -37,26 +37,66 @@ class Value:
         self.label = label
 
     def __repr__(self):
-        return f"Value(data={self.data})"
+        if self.label:
+            return f"Value(data={self.data}, Grad={self.grad}, Label={self.label})"
+        else:
+            return f"Value(data={self.data}, Grad={self.grad})"
     
     def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out =  Value(self.data + other.data, (self, other), '+')
 
         def _backward():
-            self.grad = 1.0 * out.grad
-            other.grad = 1.0 * out.grad
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
         out._backward = _backward
 
         return out
     
+    def __neg__(self):
+        return self * -1
+    
+    def __sub__(self, other):
+        return self + (-other)
+    
+    def __rsub__(self, other):
+        return other + (-self)
+    
+    def __rmul__(self, other):
+        return self * other
+    
+    def __truediv__(self, other):
+        return self * other**-1
+    
     def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self, other), '*')
 
         def _backward():
-            self.grad = other.data * out.grad
-            other.grad = self.data * out.grad
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
         out._backward = _backward
         
+        return out
+    
+    def __pow__(self, other):
+        assert(isinstance(other, (int, float)))
+        out = Value(self.data**other, (self, ), f'**{other}')
+
+        def _backward():
+            self.grad += (other*self.data**(other-1)) * out.grad
+        self._backward = _backward
+
+        return out
+    
+    def exp(self):
+        x = self.data
+        out = Value(math.exp(x), (self, ), 'exp')
+
+        def _backward():
+            self.grad += out.data * out.grad
+        out._backward = _backward
+
         return out
 
     def  tanh(self):
@@ -65,7 +105,7 @@ class Value:
         out =  Value(t, (self, ), 'tanh')
 
         def _backward():
-            self.grad = (1 - t**2) * out.grad
+            self.grad += (1 - t**2) * out.grad
         out._backward = _backward
        
         return out
@@ -78,6 +118,7 @@ class Value:
             if v not in visited:
                 visited.add(v)
                 for child in v._prev:
+                    print(child)
                     build_topo(child)
                 topo.append(v)
         build_topo(self)
